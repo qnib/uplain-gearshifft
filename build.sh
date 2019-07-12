aa5c3e54f204e4d736595fa03c357ef9e01e065a
Christian Kniep <christian@qnib.org>
2019-07-12 12:35:17 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-for x in core2 nehalem sandybridge ivybridge haswell skylake;do
-  docker build -t qnib/uplain-gearshifft:build-${x} --target=build --build-arg=CFLAGS_MARCH=${x} .
-  docker build -t qnib/uplain-gearshifft:${x} --build-arg=CFLAGS_MARCH=${x} .
+DATE=$(date +%F)
+MARCHS="core2,nehalem,sandybridge,ivybridge,haswell,skylake,znver1,znver2"
+NUM_THREADS=$(lscpu -J |jq -r '.lscpu[] | select(.field=="CPU(s):")|.data')
+echo ">> Use NUM_THREADS=${NUM_THREADS}"
+for x in $(echo ${MARCHS} |sed -e 's/,/ /g');do
+  docker build -t qnib/uplain-gearshifft:build-${x}.${DATE} --target=build \
+               --build-arg=CFLAGS_MARCH=${x} \
+		           --build-arg=NUM_THREADS=${NUM_THREADS} .
+  docker build -t qnib/uplain-gearshifft:${x}.${DATE} \
+               --build-arg=CFLAGS_MARCH=${x} \
+		           --build-arg=NUM_THREADS=${NUM_THREADS} .
+  if [[ "X$1" == "Xpush" ]];then
+    docker push qnib/uplain-gearshifft:${x}.${DATE}
+  fi
 done
